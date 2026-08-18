83f53d2a4164e7fe83e584ad724f6f5c85e12d37
Genesis <genesis@Genesiss-MacBook-Air.local>
2026-08-18 14:53:58 +0530

    Update Google Tag Manager container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,16 +98,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${manrope.variable} ${inter.variable} ${playfair.variable}`}>
-      <body className="antialiased">
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16665276342" strategy="afterInteractive" />
-        <Script id="google-ads-tag" strategy="afterInteractive">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16665276342');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PT92KW4B');
           `}
         </Script>
+      </head>
+      <body className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PT92KW4B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
